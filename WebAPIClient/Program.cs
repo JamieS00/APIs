36499b909e5c56fba40f93adbd51281c48e6a526
Jamie <Jamie@Jamies-MacBook-Air-2.local>
2023-02-14 18:50:37 -0500

    add WebAPI

---
+++ WebAPIClient/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Net.Http;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+
+
+namespace MyApp 
+    
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+        }
+    }
+}
